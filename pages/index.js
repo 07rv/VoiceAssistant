de5d54c02adf4300f7f3ff2cb622ca200af7f84c
Rohit Verma <28vrohit@gmail.com>
2023-10-24 19:03:40 +0530

    .

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Voice from "@/components/Voice";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>RvTrium</title>
+      </Head>
+      <Voice />
+    </>
+  );
+}
